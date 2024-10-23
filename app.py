291d0c0c665380c2b5715ee452de1cfca4f58ef5
ampiiarias <aarias@escuelasproa.edu.ar>
2024-10-23 14:16:39 -0300

    Cargar y Eliminar

--- app.py
+++ app.py
@@ -32,6 +32,33 @@ def clientes():
   clientes = cursor.fetchall()
   return render_template('clientes.html',clientes=clientes)
 
+@app.route('/agregar_clientes', methods=['POST'])
+def agregar_clientes():
+  #Obtengo los datos del formulario
+  dni = request.form.get('dni')
+  nombre = request.form.get('nombre')
+  apellido = request.form.get('apellido')
+  direccion = request.form.get('direccion')
+  contacto = request.form.get('contacto')
+
+  #los agrego a la base de datos
+  query = 'INSERT INTO Cliente (dni, nombre, apellido, direccion, contacto) VALUES (%s, %s, %s, %s, %s)'
+  cursor.execute(query, (dni, nombre, apellido, direccion, contacto))
+  conexion.commit()
+  return redirect(url_for('clientes'))
+
+@app.route('/eliminar_clientes', methods=['POST'])
+def eliminar_clientes():
+
+  #Obtengo el id que puso en el formulario
+  ID_cliente = request.form.get('ID')
+
+  #Hago la query en la base de datos para eliminar el cliente de ese ID
+  query = 'DELETE FROM Cliente WHERE '+ID_cliente+' = Cliente.ID_Cliente'
+  cursor.execute(query)
+  conexion.commit()
+  return redirect(url_for('clientes'))
+
 #menu productos
 @app.route('/productos')
 def productos():
